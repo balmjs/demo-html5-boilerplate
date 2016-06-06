64e1a313e5755e937c7843c05ece31b7a3d96b9d
elf-mouse <ifmouz@gmail.com>
2016-06-06 11:22:09 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var balm = require('balm');
+
+balm.config = {
+  project: 'static',
+  roots: {
+    app: 'app'
+  },
+  paths: {
+    app: {
+      css: '/css',
+      js: '/js',
+      img: '/img'
+    }
+  },
+  scripts: {
+    entry: {
+      'home': './app/js/home.js',
+      'about': './app/js/about.js'
+    }
+  }
+};
+
+balm.go();
